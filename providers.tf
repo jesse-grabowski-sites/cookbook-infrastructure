f74638f0a2f811f5edf89f963f970bf4bc3e2138
jesse-grabowski <github@jessegrabowski.com>
2025-11-11 17:35:56 +0900

    Set use_path_style

--- providers.tf
+++ providers.tf
@@ -17,6 +17,7 @@ terraform {
     skip_requesting_account_id  = true
     skip_metadata_api_check     = true
     skip_s3_checksum            = true
+    use_path_style              = true
   }
 }
 
